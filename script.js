e1877759650b9ccdfdf70d0b425d43c2c9a999f6
miuhuru <zermoun.yasmine@gmail.com>
2022-02-21 21:12:08 +0100

    update

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 "use strict";
 
-
-const btn = document.querySelector('.btn-country');
 const container = document.querySelector('.countries');
 
 const request = new XMLHttpRequest()
 request.open("GET", "https://restcountries.com/v2/name/algeria")
 
 request.send();
-console.log(request.responseText)
 
 request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText)
-    console.log(data)
+  const [data] = JSON.parse(this.responseText)
+  console.log(data)
 
-    const html = ` <article class="country">
+  const html = ` <article class="country">
 <img class="country__img" src="${data.flag}" />
 <div class="country__data">
   <h3 class="country__name">${data.name}</h3>
@@ -22,9 +19,7 @@ request.addEventListener("load", function () {
   <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
   <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
 </div>
-</article> 
-`
-    
-    container.insertAdjacentHTML("beforeend", html)
+</article> `
+container.insertAdjacentHTML("beforeend", html)
 })
 
