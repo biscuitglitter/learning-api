1ba65a2aa0df1bee9a6d2bc9f6cf3f27e4dd3bb2
miuhuru <zermoun.yasmine@gmail.com>
2022-02-22 18:56:45 +0100

    writing api call for rest countries api

--- script.js
+++ script.js
@@ -2,24 +2,25 @@
 
 const container = document.querySelector('.countries');
 
-const request = new XMLHttpRequest()
-request.open("GET", "https://restcountries.com/v2/name/algeria")
-
-request.send();
-
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText)
-  console.log(data)
-
-  const html = ` <article class="country">
-<img class="country__img" src="${data.flag}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-</div>
-</article> `
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+          <h4 class="country__name">Name: ${data.name}</h4>
+          <h4 class="country__capital">Capital: ${data.capital}</h4>
+       </div>
+  </article>
+`
 container.insertAdjacentHTML("beforeend", html)
-})
+}
+
+const getCountryData = (country) => {
+  fetch(`https://restcountries.com/v2/name/${country}`).then(
+    response => response.json()).then(
+      data => renderCountry(data[0])
+    ).then(data => renderCountry(data, "neighbour"))
+}
+
+getCountryData("algeria")
 
